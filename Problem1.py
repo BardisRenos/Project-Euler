5f6ec93893c700cd14655582d0f39408e3a6bf8b
Renos Bardis <renosbardis@gmail.com>
2020-03-14 19:57:57 +0100

    The new verison with unit test

--- Problem1.py
+++ Problem1.py
@@ -4,20 +4,35 @@
 class Problem1:
 
     def multipleOf(self, num):
-        listOfSum = []
+        if(type(num) is str):
+            return False
+        if(num < 1):
+            return False
+        else:    
+            listOfSum = []
+            for i in range(1, num):
+                if(i%3 == 0 or i%5 == 0):
+                    listOfSum.append(i)
 
-        for i in range(1, num):
-            if(i%3 == 0 or i%5 == 0):
-                listOfSum.append(i)
-
-        return sum(listOfSum)
-
+            return sum(listOfSum)
 
 #Unit testing 
 import unittest
-class
+class test_Problem1(unittest.TestCase):
 
+    def test_values(self):
+        #Test of the given value
+        A = Problem1()
+        self.assertFalse(A.multipleOf("10"))
+    
+    def test_zero_or_negative_value(self):
+        #Test if the given value is zero or negative
+        A = Problem1()
+        self.assertFalse(A.multipleOf(0))
+
+    def test_result(self):
+        A = Problem1()
+        self.assertEqual(A.multipleOf(10), 23)
 
 if __name__ == "__main__":
-    A = Problem1()
-    print(A.multipleOf(10))
+    unittest.main()
