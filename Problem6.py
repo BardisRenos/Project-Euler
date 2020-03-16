c7c5b0070d091bc09a2fafab3ecc1a8869de9e2d
Renos Bardis <renosbardis@gmail.com>
2020-03-16 20:12:15 +0100

    the commit from the command line

--- Problem6.py
+++ Problem6.py
@@ -3,13 +3,11 @@
 
 class Problem6:
     def sumSquareDifference(self, num):
-
         sum_of_squares = 0
         square_of_sum = 0
         for i in range(1, num+1):
             sum_of_squares += i**2
             square_of_sum += i
-        
         square_of_sum = square_of_sum**2
 
         return square_of_sum-sum_of_squares
